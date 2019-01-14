29aae630480b9118557ad43cf8034588e19fc4df
Arraazzii <mumaraziz2014@gmail.com>
2019-01-15 01:44:42 +0700

    a

--- application/views/admin/template/main_js.php
+++
@@ -1,9 +0,0 @@
- 		<script src="<?php echo base_url();?>assets/vendors/jquery/dist/jquery.min.js"></script>
- 		<script src="<?php echo base_url();?>assets/vendors/popper.js/dist/umd/popper.min.js"></script>
- 		<script src="<?php echo base_url();?>assets/vendors/bootstrap/dist/js/bootstrap.min.js"></script>
- 		<script src="<?php echo base_url();?>assets/custom/js/main.js"></script>
- 		<!--  Chart js -->
- 		<script src="<?php echo base_url('assets/vendors/highchart/highcharts.js');?>"></script>
-        <script src="<?php echo base_url('assets/vendors/highchart/exporting.js');?>"></script>
-        <script src="<?php echo base_url('assets/vendors/highchart/offline-exporting.js');?>"></script>
-        <script src="<?php echo base_url('assets/vendors/highchart/export-data.js');?>"></script>
